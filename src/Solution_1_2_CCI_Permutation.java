c8db39649a800ed355fffecdfa0712167843422e
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2019-03-24 14:10:25 +0100

    1.2 CCI

---
+++ src/Solution_1_2_CCI_Permutation.java
@@ -0,0 +1,33 @@
+public class Solution_1_2_CCI_Permutation {
+    public static boolean solution(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            throw new RuntimeException("");
+        }
+
+        // It will mark those chars that are found on input strings
+        int[] charMark = new int[128];
+
+        int length1 = str1.length();
+        int length2 = str2.length();
+
+        int numCharsNotZero = length1;
+
+        for (int j = 0; j < length1; j++) {
+            charMark[str1.charAt(j)]++;
+        }
+
+        for (int j = 0; j < length2; j++) {
+            char c = str2.charAt(j);
+
+            charMark[c]--;
+            if (charMark[c] == 0) {
+                numCharsNotZero--;
+            }
+            else {
+                numCharsNotZero++;
+            }
+        }
+
+        return numCharsNotZero == 0;
+    }
+}
---
+++ test/Test_1_2_CCI_Permutation.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test_1_2_CCI_Permutation {
+    @Test
+    void test0() {
+        String str1 = "pale";
+        String str2 = "pale";
+
+        assertEquals(true, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+    @Test
+    void test1() {
+        String str1 = "alep";
+        String str2 = "pale";
+
+        assertEquals(true, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+    @Test
+    void test2() {
+        String str1 = "alep";
+        String str2 = "plae";
+
+        assertEquals(true, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+    @Test
+    void test3() {
+        String str1 = "prle";
+        String str2 = "pale";
+
+        assertEquals(false, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+
+    @Test
+    void test4() {
+        String str1 = "";
+        String str2 = "p";
+
+        assertEquals(false, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+    @Test
+    void test5() {
+        String str1 = "";
+        String str2 = "";
+
+        assertEquals(true, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+    @Test
+    void test6() {
+        String str1 = "p";
+        String str2 = "";
+
+        assertEquals(false, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+    @Test
+    void test7() {
+        String str1 = "p";
+        String str2 = "px";
+
+        assertEquals(false, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+
+    @Test
+    void test8() {
+        String str1 = "px";
+        String str2 = "p";
+
+        assertEquals(false, Solution_1_2_CCI_Permutation.solution(str1,str2));
+    }
+}
