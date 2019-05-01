f64c14fcbe1f35c815d23f262b4f5c76ec15fdc6
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2019-05-01 17:38:39 +0200

    Optimizing imports

--- src/Chapter2/Solution_2_3_CCI_DeleteMiddleNode.java
+++ src/Chapter2/Solution_2_3_CCI_DeleteMiddleNode.java
@@ -1,8 +1,5 @@
 package Chapter2;
 
-import java.util.HashSet;
-import java.util.Set;
-
 // Cracking the coding interview exercise 2.1
 public class Solution_2_3_CCI_DeleteMiddleNode {
     public static void solution(IntegerLinkedList list, LinkedListNode<Integer> middleNode) {
