4655687cb0697a711bde103d5ed51884376c5b19
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2019-05-26 20:08:12 +0200

    4.3 List of Depths

---
+++ src/Chapter4/Solution_4_3_CCI_List_Of_Depths.java
@@ -0,0 +1,33 @@
+package Chapter4;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+// Given two nodes of a binary tree finds the common ancestor
+// Assumption is that both nodes actually exist within the tree
+public class Solution_4_3_CCI_List_Of_Depths {
+    public static List<LinkedList<BinaryTreeNode<Integer>>> solution(BinaryTreeNode<Integer> root) {
+        List<LinkedList<BinaryTreeNode<Integer>>> out = new ArrayList<>();
+
+        listNodes(root, 1, out);
+
+        return out;
+    }
+
+    public static void listNodes(BinaryTreeNode<Integer> start, int level, List<LinkedList<BinaryTreeNode<Integer>>> list) {
+        if (start != null) {
+            LinkedList<BinaryTreeNode<Integer>> nodeList = null;
+            if (list.size() > level - 1) {
+                nodeList = list.get(level - 1);
+            } else {
+                nodeList = new LinkedList<>();
+                list.add(level - 1, nodeList);
+            }
+
+            nodeList.add(start);
+            listNodes(start.left, level + 1, list);
+            listNodes(start.right, level + 1, list);
+        }
+    }
+}
---
+++ test/Chapter4/Test_4_3_CCI_List_Of_Depths.java
@@ -0,0 +1,78 @@
+package Chapter4;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// Builds a Binary Search Tree (with minimal height) from a sorted array (increasing order)
+public class Test_4_3_CCI_List_Of_Depths {
+    private String tree = "1\n1->2,3\n2->4,5\n3->6,7\n4->45\n45->80,90";
+
+    private void assertList(List<LinkedList<BinaryTreeNode<Integer>>> list, int size, int[] sizes, int[][] values) {
+        assertEquals(size, list.size());
+
+        for (int j = 0; j < sizes.length; j++) {
+            assertEquals(sizes[j], list.get(j).size());
+        }
+
+        for (int j = 0; j < values.length; j++) {
+            LinkedList<BinaryTreeNode<Integer>> nodeList = list.get(j);
+            Iterator<BinaryTreeNode<Integer>> it = nodeList.iterator();
+            int arrayIndex = 0;
+            while (it.hasNext()) {
+                assertEquals(values[j][arrayIndex++], it.next().value);
+            }
+        }
+    }
+
+    @Test
+    void test0() {
+        TestBinaryTree testTree = TestBinaryTreeBuilder.build(tree);
+
+        BinaryTreeNode<Integer> root = testTree.root;
+
+        // Check that the tree has been correctly built
+        assertEquals("1,2,4,45,80,90,5,3,6,7", testTree.preOrder());
+
+        List<LinkedList<BinaryTreeNode<Integer>>> list = Solution_4_3_CCI_List_Of_Depths.solution(root);
+
+        int[] sizes = {1, 2, 4, 1, 2};
+        int[][] values = {{1}, {2, 3}, {4, 5, 6, 7}, {45}, {80, 90}};
+
+        assertList(list, 5, sizes, values);
+    }
+
+    @Test
+    void test1() {
+        String tree = "1";
+        TestBinaryTree testTree = TestBinaryTreeBuilder.build(tree);
+
+        BinaryTreeNode<Integer> root = testTree.root;
+
+        List<LinkedList<BinaryTreeNode<Integer>>> list = Solution_4_3_CCI_List_Of_Depths.solution(root);
+
+        int[] sizes = {1};
+        int[][] values = {{1}};
+
+        assertList(list, 1, sizes, values);
+    }
+
+    @Test
+    void test2() {
+        String tree = "1\n1->2\n2->3\n3->4";
+        TestBinaryTree testTree = TestBinaryTreeBuilder.build(tree);
+
+        BinaryTreeNode<Integer> root = testTree.root;
+
+        List<LinkedList<BinaryTreeNode<Integer>>> list = Solution_4_3_CCI_List_Of_Depths.solution(root);
+
+        int[] sizes = {1, 1, 1, 1};
+        int[][] values = {{1}, {2}, {3}, {4}};
+
+        assertList(list, 4, sizes, values);
+    }
+}
