329950790eea35222e9b2dd66cadcd6977e2e1f5
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2019-06-16 11:57:15 +0200

    We can be heroes. One more. Check subtree

---
+++ src/Chapter4/Solution_4_10_CCI_Check_Subtree.java
@@ -0,0 +1,117 @@
+package Chapter4;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// CCI Check if T2 is a subtree of T1
+public class Solution_4_10_CCI_Check_Subtree {
+
+    public static boolean solution(BinaryTreeNode<Integer> rootT1, BinaryTreeNode<Integer> rootT2) {
+        boolean out = false;
+
+        BinaryTreeNode<Integer> startNode = findNode(rootT1, rootT2);
+
+        if (startNode != null) {
+            out = areTreesEqual(startNode, rootT2);
+        }
+
+        return out;
+    }
+
+    // A breadth-first search is gonna be done so that we find in T1 the root of T2
+    // Returns null if T2 is not found within T1
+    private static BinaryTreeNode<Integer> findNode(BinaryTreeNode<Integer> rootT1, BinaryTreeNode<Integer> rootT2) {
+        BinaryTreeNode<Integer> out = null;
+
+        Queue<BinaryTreeNode<Integer>> nodes = new LinkedList<>();
+
+        nodes.add(rootT1);
+
+        boolean found = false;
+
+        while (!nodes.isEmpty() && !found) {
+            BinaryTreeNode<Integer> node = nodes.remove();
+
+            if (isEqualNode(node, rootT2)) {
+                found = true;
+                out = node;
+            }
+
+            if (node.left != null) {
+                nodes.add(node.left);
+            }
+
+            if (node.right != null) {
+                nodes.add(node.right);
+            }
+        }
+
+        return out;
+    }
+
+    // Compares two trees. Returns true if T2 is equal to T1
+    private static boolean areTreesEqual(BinaryTreeNode<Integer> subTreeOfT1, BinaryTreeNode<Integer> T2) {
+        Queue<BinaryTreeNode<Integer>> nodes1 = new LinkedList<>();
+        Queue<BinaryTreeNode<Integer>> nodes2 = new LinkedList<>();
+
+        nodes1.add(subTreeOfT1);
+        nodes2.add(T2);
+
+        boolean inequalityFound = false;
+
+        while (!nodes1.isEmpty() && !nodes2.isEmpty()) {
+            BinaryTreeNode<Integer> node1 = nodes1.poll();
+            BinaryTreeNode<Integer> node2 = nodes2.poll();
+
+            if (!isEqualNode(node1, node2)) {
+                inequalityFound = true;
+                break;
+            }
+
+            if (node1.left != null) {
+                nodes1.add(node1.left);
+            }
+
+            if (node2.left != null) {
+                nodes2.add(node2.left);
+            }
+
+
+            if (node1.right != null) {
+                nodes1.add(node1.right);
+            }
+
+            if (node2.right != null) {
+                nodes2.add(node2.right);
+            }
+        }
+
+        if (!nodes1.isEmpty() || !nodes2.isEmpty()) {
+            return false;
+        }
+
+        return !inequalityFound;
+    }
+
+    private static boolean isEqualNode(BinaryTreeNode<Integer> n1, BinaryTreeNode<Integer> n2) {
+        boolean out = false;
+        if (n1 == n2) {
+            out = true;
+        }
+
+        if (n1 != null && n2 != null && n1.value == n2.value) {
+            if (n1.parent != null && n2.parent != null) {
+                if ((n1.parent.left == n1 && n2.parent.left == n2) ||
+                        (n1.parent.right == n1 && n2.parent.right == n2)) {
+                    out = true;
+                }
+            }
+            // The case where the root node of the second tree appears
+            else if (n2.parent == null) {
+                out = true;
+            }
+        }
+
+        return out;
+    }
+}
---
+++ test/Chapter4/Test_4_10_CCI_Check_Subtree.java
@@ -0,0 +1,120 @@
+package Chapter4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class Test_4_10_CCI_Check_Subtree {
+
+    @Test
+    void test0() {
+        String tree1 = "2\n";
+
+        TestBinaryTree testTree = TestBinaryTreeBuilder.build(tree1);
+
+        BinaryTreeNode<Integer> root = testTree.root;
+
+        assertTrue(Solution_4_10_CCI_Check_Subtree.solution(root, root));
+    }
+
+    @Test
+    void test1() {
+        String tree1 = "2\n2->3,4\n3->,5";
+
+        TestBinaryTree testTree = TestBinaryTreeBuilder.build(tree1);
+
+        BinaryTreeNode<Integer> root = testTree.root;
+
+        assertTrue(Solution_4_10_CCI_Check_Subtree.solution(root, testTree.nodes.get(3)));
+    }
+
+    @Test
+    void test2() {
+        String tree1 = "2\n2->3,4\n3->,5";
+
+        TestBinaryTree testTree = TestBinaryTreeBuilder.build(tree1);
+
+        BinaryTreeNode<Integer> root = testTree.root;
+
+        assertTrue(Solution_4_10_CCI_Check_Subtree.solution(root, testTree.nodes.get(5)));
+    }
+
+    @Test
+    void test3() {
+        String tree1 = "2\n2->3,4\n3->,5";
+
+        TestBinaryTree testTree = TestBinaryTreeBuilder.build(tree1);
+
+        BinaryTreeNode<Integer> root = testTree.root;
+
+        assertTrue(Solution_4_10_CCI_Check_Subtree.solution(root, testTree.nodes.get(5)));
+    }
+
+    @Test
+    void test4() {
+        String tree1 = "18\n18->2\n2->3,4\n3->,5";
+        String tree2 = "2\n2->3,4\n3->,5";
+
+        TestBinaryTree testTree1 = TestBinaryTreeBuilder.build(tree1);
+        TestBinaryTree testTree2 = TestBinaryTreeBuilder.build(tree2);
+
+        assertTrue(Solution_4_10_CCI_Check_Subtree.solution(testTree1.root, testTree2.root));
+    }
+
+    @Test
+    void test5() {
+        String tree1 = "18\n18->2\n2->3,4\n3->,5";
+        String tree2 = "2\n2->3,4\n3->12,5";
+
+        TestBinaryTree testTree1 = TestBinaryTreeBuilder.build(tree1);
+        TestBinaryTree testTree2 = TestBinaryTreeBuilder.build(tree2);
+
+        assertFalse(Solution_4_10_CCI_Check_Subtree.solution(testTree1.root, testTree2.root));
+    }
+
+    @Test
+    void test6() {
+        String tree1 = "2\n";
+        String tree2 = "2\n";
+
+        TestBinaryTree testTree1 = TestBinaryTreeBuilder.build(tree1);
+        TestBinaryTree testTree2 = TestBinaryTreeBuilder.build(tree2);
+
+        assertTrue(Solution_4_10_CCI_Check_Subtree.solution(testTree1.root, testTree2.root));
+    }
+
+    @Test
+    void test7() {
+        String tree1 = "2\n";
+        String tree2 = "3\n";
+
+        TestBinaryTree testTree1 = TestBinaryTreeBuilder.build(tree1);
+        TestBinaryTree testTree2 = TestBinaryTreeBuilder.build(tree2);
+
+        assertFalse(Solution_4_10_CCI_Check_Subtree.solution(testTree1.root, testTree2.root));
+    }
+
+    @Test
+    void test8() {
+        String tree1 = "2\n";
+        String tree2 = "2\n2->,3";
+
+        TestBinaryTree testTree1 = TestBinaryTreeBuilder.build(tree1);
+        TestBinaryTree testTree2 = TestBinaryTreeBuilder.build(tree2);
+
+        assertFalse(Solution_4_10_CCI_Check_Subtree.solution(testTree1.root, testTree2.root));
+    }
+
+    @Test
+    void test9() {
+        String tree1 = "2\n2->,3";
+        String tree2 = "2\n";
+
+        TestBinaryTree testTree1 = TestBinaryTreeBuilder.build(tree1);
+        TestBinaryTree testTree2 = TestBinaryTreeBuilder.build(tree2);
+
+        assertFalse(Solution_4_10_CCI_Check_Subtree.solution(testTree1.root, testTree2.root));
+    }
+}
