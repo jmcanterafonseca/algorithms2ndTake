38454c5c23f5211308aec70857baf22532f54e7a
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2019-06-22 17:19:24 +0200

    Refactor weighted graph

--- test/Chapter4/TestGraph.java
+++ test/Chapter4/TestGraph.java
@@ -2,30 +2,30 @@ package Chapter4;
 
 import java.util.*;
 
-public class TestGraph {
-    public Map<Integer, GraphNode<Integer>> nodes;
+public class TestGraph<T> {
+    public Map<T, GraphNode<T>> nodes;
 
-    public TestGraph(Map<Integer, GraphNode<Integer>> nodes) {
+    public TestGraph(Map<T, GraphNode<T>> nodes) {
         this.nodes = nodes;
     }
 
-    public GraphNode<Integer> start() {
+    public GraphNode<T> start() {
         return nodes.values().iterator().next();
     }
 
     public String listDepthFirst() {
         StringBuffer out = new StringBuffer();
-        Set<GraphNode<Integer>> visited = new HashSet<>();
+        Set<GraphNode<T>> visited = new HashSet<>();
 
         listDepthFirst(start(), out, visited);
 
         return out.toString();
     }
 
-    private void listDepthFirst(GraphNode<Integer> start, StringBuffer out, Set<GraphNode<Integer>> visited) {
+    private void listDepthFirst(GraphNode<T> start, StringBuffer out, Set<GraphNode<T>> visited) {
         visit(start, out, visited);
 
-        for (GraphNode<Integer> node : start.adjacentNodes) {
+        for (GraphNode<T> node : start.adjacentNodes) {
             if (!visited.contains(node)) {
                 listDepthFirst(node, out, visited);
                 out.append(",");
@@ -37,18 +37,18 @@ public class TestGraph {
 
     public String listBreadthFirst() {
         StringBuffer out = new StringBuffer();
-        Set<GraphNode<Integer>> visited = new HashSet<>();
+        Set<GraphNode<T>> visited = new HashSet<>();
 
-        Queue<GraphNode<Integer>> toVisit = new LinkedList<>();
+        Queue<GraphNode<T>> toVisit = new LinkedList<>();
 
         toVisit.add(start());
 
         while (!toVisit.isEmpty()) {
-            GraphNode<Integer> next = toVisit.remove();
+            GraphNode<T> next = toVisit.remove();
             if (!visited.contains(next)) {
                 visit(next, out, visited);
             }
-            for (GraphNode<Integer> node : next.adjacentNodes) {
+            for (GraphNode<T> node : next.adjacentNodes) {
                 toVisit.add(node);
             }
         }
@@ -56,7 +56,7 @@ public class TestGraph {
         return out.substring(0, out.length() - 1);
     }
 
-    private void visit(GraphNode<Integer> node, StringBuffer out, Set<GraphNode<Integer>> visited) {
+    private void visit(GraphNode<T> node, StringBuffer out, Set<GraphNode<T>> visited) {
         out.append(node.value);
         out.append(",");
 
