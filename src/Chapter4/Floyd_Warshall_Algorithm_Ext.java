4a40131a6a302e7f2af1f30b31c2ccef50e0b3ca
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2019-07-07 18:35:20 +0200

    WIP Floyd-Warshall to obtain shortest path. still some cases failing

--- src/Chapter4/Floyd_Warshall_Algorithm_Ext.java
+++ src/Chapter4/Floyd_Warshall_Algorithm_Ext.java
@@ -16,31 +16,29 @@ public class Floyd_Warshall_Algorithm_Ext {
 
         Collection<GraphVertex<String>> vertices = graph.vertices.values();
 
-        // Distances Map is cloned so we left untouched the original one
-        Map<String, Map<String, Integer>> distances = (Map<String, Map<String, Integer>>)
-                ((HashMap<String, Map<String, Integer>>) graph.distances).clone();
-
         // Initialize out just in case the shortest path between a and b it is just from a to b
         for (GraphVertex<String> gv : vertices) {
             Map<String, VertexDistancePair> vdistanceMap = new HashMap<>();
             out.put(gv.label, vdistanceMap);
-            
-            Collection<String> adjacents = distances.get(gv.label).keySet();
+
+            Collection<String> adjacents = graph.distances.get(gv.label).keySet();
             for (String adjacent : adjacents) {
-                vdistanceMap.put(adjacent, new VertexDistancePair(distances.get(gv.label).get(adjacent),
+                vdistanceMap.put(adjacent, new VertexDistancePair(graph.distances.get(gv.label).get(adjacent),
                         null));
             }
+
+            vdistanceMap.put(gv.label, new VertexDistancePair(0, null));
         }
 
         for (GraphVertex<String> vertexK : vertices) {
             for (GraphVertex<String> vertexI : vertices) {
                 for (GraphVertex<String> vertexJ : vertices) {
-                    int distanceIJ = getDistance(vertexI, vertexJ, distances);
-                    int distanceIK = getDistance(vertexI, vertexK, distances);
-                    int distanceKJ = getDistance(vertexK, vertexJ, distances);
+                    int distanceIJ = getDistance(vertexI, vertexJ, out);
+                    int distanceIK = getDistance(vertexI, vertexK, out);
+                    int distanceKJ = getDistance(vertexK, vertexJ, out);
 
                     if (distanceIK + distanceKJ < distanceIJ) {
-                        setDistanceThrough(vertexI, vertexJ, vertexK, distanceIK + distanceKJ, out, distances);
+                        setDistanceThrough(vertexI, vertexJ, vertexK, distanceIK + distanceKJ, out);
                     }
                 }
             }
@@ -85,11 +83,7 @@ public class Floyd_Warshall_Algorithm_Ext {
     }
 
     private static void setDistanceThrough(GraphVertex<String> gv1, GraphVertex<String> gv2, GraphVertex<String> gv3,
-                                           int distance, Map<String, Map<String, VertexDistancePair>> out,
-                                           Map<String, Map<String, Integer>> distances) {
-
-        Map<String, Integer> distanceMap = distances.get(gv1.label);
-        distanceMap.put(gv2.label, distance);
+                                           int distance, Map<String, Map<String, VertexDistancePair>> out) {
 
         Map<String, VertexDistancePair> vtxDistanceMap = out.get(gv1.label);
         if (vtxDistanceMap == null) {
@@ -100,13 +94,13 @@ public class Floyd_Warshall_Algorithm_Ext {
     }
 
     private static int getDistance(GraphVertex<String> gv1, GraphVertex<String> gv2,
-                                   Map<String, Map<String, Integer>> distances) {
-        Map<String, Integer> distanceHash = distances.get(gv1.label);
+                                   Map<String, Map<String, VertexDistancePair>> distances) {
+        Map<String, VertexDistancePair> distanceHash = distances.get(gv1.label);
 
         int distance = INF_DISTANCE;
 
         if (distanceHash.get(gv2.label) != null) {
-            distance = distanceHash.get(gv2.label);
+            distance = distanceHash.get(gv2.label).distance;
         }
 
         return distance;
